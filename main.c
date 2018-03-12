16e1dad4b97295fb95c2b1e14e48570a168ef2eb
Пащенко Андрей Дмитриевич <ip712s17@mwp07.csc.local>
2018-03-12 09:03:18 +0700

    new program_2

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-  printf(" 8 r n b q k b n r /n 7 p p p p p p p p /n 6 /n 5 /n 4 /n 3 /n 2 P P P P P P P P /n 1 R N B Q K B N R /n a b c d e f g h");
+  printf(" 8 r n b q k b n r");
+      printf(" 7 p p p p p p p p");
+      printf(" 6 ");
+      printf(" 5 ");
+      printf(" 4 ");
+      printf(" 3 ");
+      printf(" 2 P P P P P P P P");
+      printf(" 1 R N B Q K B N R");
+    printf("   a b c d e f g h");
   return 0;
 }
